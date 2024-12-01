9a5e72e9710f4021d9a9dec4f8420c8213cdca3c
Torgeir S <web@torgeir.tk>
2024-12-01 11:42:02 +0100

    Solution day 1, part 1

--- src/tasks/01-1.ts
+++ src/tasks/01-1.ts
@@ -1,13 +1,35 @@
-import { getData, logString } from "../helpers";
+import { getData, logJson, logJsonPretty, logString } from "../helpers";
 
 // Add testdata (e.g. example data from the task) here when working out a solution.
 const testData = `
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
 `;
 let sum = 0; // Add to this when summing up stuff
-const data = getData("01-1", testData, false); // Change to true to switch to the complete data set
+const data = getData("01-1", testData, true); // Change to true to switch to the complete data set
 const lines = data.split("\n");
+const leftList: number[] = [];
+const rightList: number[] = [];
 for (const line of lines) {
   if (!line.trim()) continue; // empty line
   // Handle each line here, then add the result to sum
+  const [left, right] = line.split("   ");
+  leftList.push(parseInt(left as string));
+  rightList.push(parseInt(right as string));
+}
+leftList.sort();
+rightList.sort();
+for (let i = 0; i < leftList.length; i++) {
+  const left = leftList[i];
+  const right = rightList[i];
+  if (left === undefined || right === undefined) {
+    throw new Error("Should not happen");
+  }
+  sum += Math.abs(left - right);
 }
 logString("Result: " + sum);
+
